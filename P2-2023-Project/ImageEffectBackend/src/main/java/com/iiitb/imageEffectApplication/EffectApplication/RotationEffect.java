211a3313856f3cc0cb5b0d3ecb9979a2ceb8255b
Uttam <uttamhamsaraj24@gmail.com>
2024-01-08 00:11:56 +0530

    RotationEffect.java

--- P2-2023-Project/ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/EffectApplication/RotationEffect.java
+++ P2-2023-Project/ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/EffectApplication/RotationEffect.java
@@ -1,29 +1,3 @@
-// package com.iiitb.imageEffectApplication.effectImplementation;
-
-// import com.iiitb.imageEffectApplication.service.LoggingService;
-// import com.iiitb.imageEffectApplication.libraryInterfaces.Pixel;
-// import com.iiitb.imageEffectApplication.libraryInterfaces.RotaionInterface;
-// import com.iiitb.imageEffectApplication.libraryInterfaces.RotationInterface;
-// import com.iiitb.imageEffectApplication.baseEffects.PhotoEffect;
-// import com.iiitb.imageEffectApplication.exception.IllegalParameterException;
-// import com.iiitb.imageEffectApplication.baseEffects.DiscreteEffect;
-// public class RotationEffect implements DiscreteEffect{
-//     int variable = 0;
-//     loggingService.addLog(fileName, "Rotation", Int.toString(value));
-
-//     public Pixel[][] apply(Pixel [][]inputImage, String imageName,LoggingService loggingService){
-//         return RotationInterface.applyRotation(inputImage,variable);
-//     }
-//     public void selectOptionValue(String optionName, int value) throws IllegalParameterException{
-//         if (value instanceof Float){
-//             throw new IllegalParameterException();
-//         }
-//         else{
-//             variable = value;
-//         }
-//     }
-
-// }
 
 package com.iiitb.imageEffectApplication.EffectApplication;
 import com.iiitb.imageEffectApplication.service.LoggingService;
