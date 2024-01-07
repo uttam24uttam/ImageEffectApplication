76b07d4dfe19d78ec854f01c30000c7e1bd4a136
Uttam <uttamhamsaraj24@gmail.com>
2024-01-08 00:07:02 +0530

    HueSaturationEffect.java

--- P2-2023-Project/ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/EffectApplication/HueSaturationEffect.java
+++ P2-2023-Project/ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/EffectApplication/HueSaturationEffect.java
@@ -1,41 +1,4 @@
-// package com.iiitb.imageEffectApplication.effectImplementation;  
 
-// import com.iiitb.imageEffectApplication.service.LoggingService;
-// import com.iiitb.imageEffectApplication.libraryInterfaces.Pixel;
-// import com.iiitb.imageEffectApplication.libraryInterfaces.HueSaturationInterface;
-// import com.iiitb.imageEffectApplication.baseEffects.ParameterizableEffect;
-// import com.iiitb.imageEffectApplication.exception.IllegalParameterException;
-
-// public class HueSaturationEffect implements ParameterizableEffect{
-//     private float val = 0.0f;
-//     loggingService.addLog(imageName, "HueSaturation", "hueAmount:" + Float.toString(hueAmount) + " saturationAmount:" + Float.toString(saturationAmount));
-
-//     public Pixel[][] apply(Pixel[][] image, float hueval, float satval){
-//         return HueSaturationInterface.HueSaturationImageEffect(image, hueval, satval);
-//     }
-
-//     public void setParameter(String paramName, float value) throws IllegalParameterException{
-//         if(paramName.equals("hueAmt")){
-//             if(value>=0 && value<=100){
-//                 val = value;
-//             }
-//             else{
-//                 throw new IllegalParameterException();
-//             }
-//         }
-//         else if(paramName.equals("satAmt")){
-//             if(value>=0 && value<=100){
-//                 val = value;
-//             }
-//             else{
-//                 throw new IllegalParameterException();
-//             }
-//         }
-//         else{
-//             throw new IllegalParameterException();
-//         }
-//     }                 
-// }
 package com.iiitb.imageEffectApplication.EffectApplication;
 import com.iiitb.imageEffectApplication.service.LoggingService;
 import com.iiitb.imageEffectApplication.libraryInterfaces.Pixel; 
