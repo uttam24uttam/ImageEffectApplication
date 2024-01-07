72ce9dcaafd2d4958c59539b645caf9b4cd5d660
Uttam <uttamhamsaraj24@gmail.com>
2024-01-08 00:12:30 +0530

    GrayscaleEffect.java

--- P2-2023-Project/ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/EffectApplication/GrayscaleEffect.java
+++ P2-2023-Project/ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/EffectApplication/GrayscaleEffect.java
@@ -6,14 +6,6 @@ import com.iiitb.imageEffectApplication.baseEffects.PhotoEffect;
 
 public class GrayscaleEffect implements PhotoEffect {
 
-    // private final LoggingService loggingService;
-    // private final String fileName;
-
-    // public GrayscaleEffect(LoggingService loggingService, String fileName) {
-    //     this.loggingService = loggingService;
-    //     this.fileName = fileName;
-    //}
-
     public Pixel[][] apply(Pixel[][] image, String fileName, LoggingService loggingService) {
         loggingService.addLog(fileName, "Grayscale", "");
         return GrayscaleInterface.applyGrayscale(image);
