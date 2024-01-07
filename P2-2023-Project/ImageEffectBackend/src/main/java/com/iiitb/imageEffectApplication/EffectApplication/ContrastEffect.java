641f500061ca466aff9f7cd8ab6e84b5ea4d66e4
Uttam <uttamhamsaraj24@gmail.com>
2024-01-08 00:06:08 +0530

    ContrastEffect.java

--- P2-2023-Project/ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/EffectApplication/ContrastEffect.java
+++ P2-2023-Project/ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/EffectApplication/ContrastEffect.java
@@ -1,26 +1,3 @@
-// package com.iiitb.imageEffectApplication.effectImplementation;
-
-// import com.iiitb.imageEffectApplication.service.LoggingService;
-// import com.iiitb.imageEffectApplication.libraryInterfaces.Pixel;
-// import com.iiitb.imageEffectApplication.libraryInterfaces.ContrastInterface;
-// import com.iiitb.imageEffectApplication.baseEffects.SingleValueParameterizableEffect;
-// import com.iiitb.imageEffectApplication.exception.IllegalParameterException;
-// public class contrastEffect implements SingleValueParameterizableEffect{
-//     private float variable = 0;
-//     loggingService.addLog(fileName, "Contrast", Float.toString(value));
-
-//     public Pixel[][] apply(Pixel[][] image, String fileName, LoggingService loggingService){
-//         return ContrastInterface.applyContrast(image,variable);
-//     }
-//     public void setParameterValue(float amount) throws IllegalParameterException{
-//         if(amount >=0 && amount <=255){
-//             variable = amount;
-//         }
-//         else{
-//             throw new IllegalParameterException();
-//         }
-//     }
-// }
 
 package com.iiitb.imageEffectApplication.EffectApplication;
 import com.iiitb.imageEffectApplication.service.LoggingService;
