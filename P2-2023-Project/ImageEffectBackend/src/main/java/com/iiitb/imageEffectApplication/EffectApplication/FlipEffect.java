e2af8977e1ef087d437dc1baf93cfe0d7ccedcb7
Uttam <uttamhamsaraj24@gmail.com>
2024-01-08 00:06:34 +0530

    FlipEffect.java

--- P2-2023-Project/ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/EffectApplication/FlipEffect.java
+++ P2-2023-Project/ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/EffectApplication/FlipEffect.java
@@ -1,35 +1,4 @@
-// package com.iiitb.imageEffectApplication.EffectImplementations;
-
-// import com.iiitb.imageEffectApplication.service.LoggingService;
-// import com.iiitb.imageEffectApplication.libraryInterfaces.Pixel;
-// import com.iiitb.imageEffectApplication.baseEffects.DiscreteEffect;
-// import com.iiitb.imageEffectApplication.libraryInterfaces.FlipInterface;
-// import com.iiitb.imageEffectApplication.exception.IllegalParameterException;
-
-
-// public class FlipEffect implements DiscreteEffect{
-//     private int v = 0,h=0;
-
-//     loggingService.addLog(fileName, "Flip", parameter);
-
-//     public Pixel[][] apply(Pixel[][] image, String fileName, LoggingService loggingService){
-//         return FlipInterface.applyFlip(image,h,v);
-//     }
-// //public interface DiscreteEffect extends PhotoEffect { 
-//     public void selectOptionValue(String optionName, int value) throws IllegalParameterException{
-//         if(optionName.equals("Horizontal")){
-//             if(value == 1 || value == 0){
-//               h=value;}}
-//         else if(optionName.equals("Vertical")){
-//             if(value == 1 || value == 0){
-//               v=value;}}      
-
-//         else
-//             throw new IllegalParameterException();
-
-//     }
-// }    
-        
+  
 package com.iiitb.imageEffectApplication.EffectApplication;
 import com.iiitb.imageEffectApplication.service.LoggingService;
 import com.iiitb.imageEffectApplication.libraryInterfaces.Pixel;
