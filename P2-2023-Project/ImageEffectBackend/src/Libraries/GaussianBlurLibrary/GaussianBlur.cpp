3ac65751922c8e260c8b00f045aa8aaea5d3d1c3
Uttam <uttamhamsaraj24@gmail.com>
2024-01-08 00:14:03 +0530

    GaussianBlur.cpp

--- P2-2023-Project/ImageEffectBackend/src/Libraries/GaussianBlurLibrary/GaussianBlur.cpp
+++ P2-2023-Project/ImageEffectBackend/src/Libraries/GaussianBlurLibrary/GaussianBlur.cpp
@@ -3,63 +3,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// void applyGaussianBlur(vector<vector<Pixel>>& image, float radius) {
-//     int height = image.size();
-//     int width = image[0].size();
-
-//     vector<vector<Pixel>> blurredImage(height, vector<Pixel>(width));
-
-//     int intRadius = static_cast<int>(ceil(radius));
-//     vector<float> kernel(2 * intRadius + 1, 0.0f);
-//     float sigma = radius / 3.0f;
-//     float sigmaSquared = 2.0f * sigma * sigma;
-//     float sum = 0.0f;
-
-//     for (int x = -intRadius; x <= intRadius; ++x) {
-//         float expVal = exp(-(x * x) / sigmaSquared);
-//         kernel[x + intRadius] = expVal;
-//         sum += expVal;
-//     }
-
-//     for (int i = 0; i < kernel.size(); ++i) {
-//         kernel[i] /= sum;
-//     }
-
-//     vector<vector<Pixel>> tempImage = image;
-
-//     for (int i = 0; i < height; ++i) {
-//         for (int j = intRadius; j < width - intRadius; ++j) {
-//             Pixel blurValue;
-//             for (int x = -intRadius; x <= intRadius; ++x) {
-//                 blurValue.r += kernel[x + intRadius] * image[i][j + x].r;
-//                 blurValue.g += kernel[x + intRadius] * image[i][j + x].g;
-//                 blurValue.b += kernel[x + intRadius] * image[i][j + x].b;
-//             }
-//             blurredImage[i][j] = blurValue;
-//         }
-//     }
-
-//     for (int i = intRadius; i < height - intRadius; ++i) {
-//         for (int j = 0; j < width; ++j) {
-//             Pixel blurValue;
-//             for (int y = -intRadius; y <= intRadius; ++y) {
-//                 blurValue.r += kernel[y + intRadius] * blurredImage[i + y][j].r;
-//                 blurValue.g += kernel[y + intRadius] * blurredImage[i + y][j].g;
-//                 blurValue.b += kernel[y + intRadius] * blurredImage[i + y][j].b;
-//             }
-//             tempImage[i][j] = blurValue;
-//         }
-//     }
-
-//     image = tempImage;
-
-// }
-
-
-// #include <vector>
-// #include <cmath>
-// #include<bits/stdc++.h> 
-// using namespace std;
 
 void gaussianBlur(const vector<vector<float>>& imageVector, float radius) {
     int size=static_cast<int>(2*radius+1);
